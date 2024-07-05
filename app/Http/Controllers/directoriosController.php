45653046283ded6fe07cceac9b48957ad621d115
IdaIvZe <idalia.zelaya@unah.hn>
2024-07-04 19:16:58 -0600

    ExamenUnidadI

---
+++ app/Http/Controllers/directoriosController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\directorio;
+
+class directoriosController extends Controller
+{
+    //
+
+    public function mostrarDirectorio(){
+        $directorios = directorio::all();
+        return view('directorio', compact('directorios'));
+    }
+
+    public function mostrarAgregarNuevo(){
+        $nvoDirectorio = directorio::all();
+        return view('crearentrada', compact('nvoDirectorio'));
+    }
+
+    public function crearNuevoDirectorio(Request $request){
+        $nvoDirectorio = new directorio();
+
+    }
+
+    public function mostrarBuscarEntrada(){
+        $buscarEntradas = directorio::all();
+        return view('buscar', compact('buscarEntradas'));
+    }
+
+    public function verContactos(){
+        $verContactos = directorio::all();
+        return view('vercontactos', compact('verContactos'));
+    }
+
+    public function mostrarEliminar(){
+        $registroEliminar = directorio::all();
+        return view('eliminar', compact('registroEliminar'));
+    }
+
+    public function eliminacionCancelar(){
+        $registrosEliminar = directorio::all();
+        return redirect()->route('directorio.principal');
+    }
+
+    public function cancelarBusqueda(){
+        return redirect()->route('directorio.principal');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\directoriosController;
+use App\Http\Controllers\contactosController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/directorio',
+            [directoriosController::class, 'mostrarDirectorio'])->name('directorio.principal');
+
+
+Route::get('/directorio/mostrar/agregarNuevo',
+            [directoriosController::class, 'mostrarAgregarNuevo'])->name('agregarNvoDirectorio.mostrar');
+
+
+Route::get('/directorio/agregar/nuevo',
+            [directoriosController::class, 'crearNuevoDirectorio'])->name('directorio.agregarNuevo');
+
+Route::get('/directorio/buscar', 
+            [directoriosController::class, 'mostrarBuscarEntrada'])->name('buscarEntradas.buscar');
+
+Route::get('/directorio/ver',
+            [directoriosController::class, 'verContactos'])->name('directorio.verContactos');
+
+Route::get('/directorio/eliminar',
+            [directoriosController::class, 'mostrarEliminar'])->name('directorio.eliminar.mostrar');
+
+Route::get('/directorio/eliminar/cancelar',
+            [directoriosController::class, 'eliminacionCancelar'])->name('directorio.eliminar.cancelar');
+
+Route::get('/directorio/buscar/regresar',
+            [directoriosController::class, 'cancelarBusqueda'])->name('directorio.buscar.regresar');
